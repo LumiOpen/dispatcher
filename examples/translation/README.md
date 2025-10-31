27dc02575aede3c80e31da0b0029f8fa32f26b2f
Adam Hrin <adamhrin@amd.com>
2025-10-31 11:17:33 +0200

    init for finnish synthetic traces eval

--- examples/translation/README.md
+++ examples/translation/README.md
@@ -10,3 +10,18 @@ There are 3 steps to run these evaluations:
 1. Translate prompts with `launch_translate_task_sing.sh` with all candidate translation models.
 2. Generate answers for each translation model with `launch_answer_task_sing.sh`.
 3. Evaluate models with `evaluate_prompts.py` or check statistically significant differences with `significance.py`. Both of these require gold standard answers in addition to the generations as the input.
+
+
+## Finnish reasoning traces quality evaluation
+
+1. Translate prompts (deepscaler) with one model - this should be fixed in this experiment (for now deepseekv3)
+    - `/scratch/project_462000353/adamhrin/dispatcher/examples/translation/data/default-train-sample-100_translations_DeepSeek-V3_fi.jsonl`
+    - `/scratch/project_462000353/adamhrin/dispatcher/examples/translation/data/default-train-sample-100_translations_Qwen2.5-72B-Instruct_fi.jsonl`
+2. Use different candidate models (non-reasoning) to generate reasoning traces + answer (although that's not interesting to us - perhaps can be affected by the prompt but in this stage we let the model generate the answer as well). The input to the model is the prompt guiding the model to generate reasoning traces + the actual math question in finnish
+
+```sh
+sbatch launch_answer_task.sh
+```
+
+3. Take a reasoning model (fixed) to get the answer given the translated prompt and generated reasoning trace. This can be one of deepseekv3, r1 or qwen3 (MoE)
+4. The accuracy of the problem solving across the models in (2) is a proxy for how well the models in (2) translated the traces.
--- examples/translation/answering_task.py
+++ examples/translation/answering_task.py
@@ -9,12 +9,10 @@ from dispatcher.taskmanager.backend.request import Request, Response
 from dispatcher.taskmanager.task.base import GeneratorTask
 from dispatcher.taskmanager.task import TaskFailed
 
-import random
 import os
-import re
 import logging
 
-__all__ = ["TranslationAnsweringTask"]
+__all__ = ["AnsweringTask"]
 
 LANGUAGE=os.environ.get("LANGUAGE")
 
@@ -51,42 +49,6 @@ LANGUAGE_NAMES = {
     "no": ["Norwegian", "nob"],
 }
 
-GENERAL_TRANSLATION_PROMPT = """
-You are a professional translator. Your task is to translate the following math problem faithfully and accurately into {language}. Translate the problem only, DO NOT answer the problem.
-
-Guidelines:
-
-1. Preserve the original meaning, tone, and nuance.
-2. Do not summarize, omit, or add information. 
-3. Keep mathematical notations, numbers, technical terms unchanged unless a standard translation exists.
-4. Maintain the style and register (formal/informal, literary/technical, etc.) of the source text.
-5. If a phrase could be translated multiple ways, choose the one that best matches the author’s intent and note any ambiguity.
-
-Do not explain your translation; output only the translated text unless asked otherwise.
-
-Problem to translate:
-{text}
-"""
-
-
-DEEPSEEK_TRANSLATION_PROMPT = """
-You are a professional translator specializing in mathematics and scientific texts. Your task is to translate the following content faithfully and accurately into {language}.
-
-Guidelines:
-
-1. Preserve all LaTeX code, equations, symbols, and formatting exactly as written.
-2. Translate only the surrounding natural language, not the math expressions inside ( ... ), [ ... ], or $$ ... $$.
-3. Maintain the precise meaning, tone, and logical structure of the original text.
-4. Use the standard mathematical terminology of the target language.
-5. Do not simplify, interpret, or solve the math; your goal is linguistic translation only.
-6. Keep variable names, constants, and notation unchanged.
-7. If an English math term has multiple valid equivalents in the target language, choose the most widely accepted in academic usage.
-8. Do not explain your translation; output only the translated text unless asked otherwise.
-
-Text to translate:
-{text}
-"""
-
 OPEN_R1_REASONING_PROMPT = """
 You are a reasoning model to help users solve complex problems. Your role as an assistant involves thoroughly exploring questions through a systematic thinking process before providing the final precise and accurate solutions. This requires engaging in a comprehensive cycle of analysis, summarizing, exploration, reassessment, reflection, backtracing, and iteration to develop well-considered thinking process. 
 Please structure your response into two main sections: 
@@ -100,19 +62,13 @@ Now, try to solve the following question through the above guidelines.
 """
 
 class AnsweringTask(GeneratorTask):
-    """Translation and reasoning trace generation."""
-    
-    TRANSLATION_GEN_PARAMS: Dict[str, Any] = {
-        "temperature": 0.0,
-        "top_p": 1.0,
-        "max_tokens": 8192,
-    }
+    """Reasoning trace + answer generation."""
     
     ANSWER_GEN_PARAMS: Dict[str, Any] = {
         "temperature": 0.6,
         "top_p": 0.95,
         "max_tokens": 14336, # Leave 2K tokens for the prompt
-    #    'n': 16, # We generate multiple answers for each prompt. # FIXME: This doesn't seem to work
+        "n": 4,
     }
 
     logger = logging.getLogger(__name__)
@@ -132,9 +88,6 @@ class AnsweringTask(GeneratorTask):
             }
         ]
         # We request multiple answers per prompt
-        answers = []
-        for _ in range(4):
-            answer_resp: Response = yield Request({"messages": input_messages, **self.ANSWER_GEN_PARAMS})
-            answers.append(answer_resp.get_text())
-        return_dict["generated_answers"] = answers
+        answer_resp: Response = yield Request({"messages": input_messages, **self.ANSWER_GEN_PARAMS})
+        return_dict["generated_answers"] = answer_resp.get_text(n=4)
         return return_dict
